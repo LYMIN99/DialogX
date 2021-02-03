c6c36b1e90c41153b90da351261bdc8df4e81c2f
lymin99 <lymin007@gmail.com>
2021-02-03 11:27:57 +0700

    a

---
+++ MyLibrary/src/main/java/com/lymin/mylibrary/DialogX.java
@@ -0,0 +1,11 @@
+package com.lymin.mylibrary;
+
+import android.util.Log;
+
+import androidx.annotation.LongDef;
+
+public class DialogX {
+    public static void show(){
+        Log.d("DialogX","It Work!");
+    }
+}
